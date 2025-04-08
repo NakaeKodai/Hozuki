a396559bf67502cb67a42e40ad6531a79a6ee51d
NakaeKodai <0uz047526ky3k4d@au.com>
2025-04-09 00:54:08 +0900

    パスワードで開くギミック

--- Assets/Script/Gimmick/PasswordLock.cs
+++ Assets/Script/Gimmick/PasswordLock.cs
@@ -1,10 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PasswordLock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    private bool inPlayer, isPlay;
+    
+    public string cursorSE = "カーソル移動";
+    public string openSE = "鍵を開ける";
+
+    [SerializeField] private GameObject lockedimage;
+    private int nowCursorNum = 0;
+    private int beforeCursorNum = 1; //nowCursorNumとは違う数値
+    private TextMeshProUGUI numberText;
+    private GameObject nowCursor;
+    private GameObject nowCursorImage;
+
+    [SerializeField] private GameObject operation;
+    [SerializeField] private TextMeshProUGUI operationText;
+
+    [SerializeField]
+    private List<int> answerNumber = new List<int>();
+
+    [SerializeField]
+    private List<int> passwordNumber = new List<int>();
+
+    // [ContextMenu("Sync With Children")]
+    // private void SyncWithChildren()
+    // {
+    //     int childCount = lockedimage.transform.childCount;
+    //     while (answerNumber.Count < childCount)
+    //     {
+    //         answerNumber.Add(0);
+    //         passwordNumber.Add(0);
+    //     }
+    //     while (answerNumber.Count > childCount)
+    //     {
+    //         answerNumber.RemoveAt(answerNumber.Count - 1);
+    //         passwordNumber.RemoveAt(passwordNumber.Count - 1);
+    //     }
+    // }
+
+
     void Start()
     {
         
@@ -13,6 +53,145 @@ public class PasswordLock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(inPlayer)
+        {
+            if(!isPlay)
+            {
+                // 入力前の処理
+
+                // 操作説明
+                if(GameManager.controllerType == GameManager.ControllerType.Unknown)
+                {
+                    operationText.text = "Space : 調べる";
+                }
+                else if(GameManager.controllerType == GameManager.ControllerType.PlayStation)
+                {
+                    operationText.text = "● : 調べる";
+                }
+                else if(GameManager.controllerType == GameManager.ControllerType.Nintendo)
+                {
+                    operationText.text = "A : 調べる";
+                }
+                else if(GameManager.controllerType == GameManager.ControllerType.Xbox)
+                {
+                    operationText.text = "B : 調べる";
+                }
+
+                if(GameManager.instance.playerInputAction.Player.ActionANDDecision.triggered)
+                {
+                    lockedimage.SetActive(true);
+                    // operation.SetActive(false);
+                    GameManager.instance.isOtherMenu = true;
+                    isPlay = true;
+                }
+            }
+            else
+            {
+                // パスワード入力の処理
+
+                // 操作説明
+                if(GameManager.controllerType == GameManager.ControllerType.Unknown)
+                {
+                    operationText.text = "W S : 数字変更　　A D : 桁変更";
+                }
+                else if(GameManager.controllerType == GameManager.ControllerType.PlayStation)
+                {
+                    operationText.text = "↑ ↓ : 数字変更　　← → : 桁変更";
+                }
+                else if(GameManager.controllerType == GameManager.ControllerType.Nintendo)
+                {
+                    operationText.text = "↑ ↓ : 数字変更　　← → : 桁変更";
+                }
+                else if(GameManager.controllerType == GameManager.ControllerType.Xbox)
+                {
+                    operationText.text = "↑ ↓ : 数字変更　　← → : 桁変更";
+                }
+
+                if(GameManager.instance.playerInputAction.UI.CloseMenu.triggered)
+                {
+                    lockedimage.SetActive(false);
+                    // operation.SetActive(true);
+                    GameManager.instance.isOtherMenu = false;
+                    isPlay = false;
+                }
+                
+
+                //カーソル変更と数字変更
+                if(beforeCursorNum != nowCursorNum)
+                {
+                    nowCursor = lockedimage.transform.GetChild(nowCursorNum).gameObject;
+                    nowCursorImage = nowCursor.transform.GetChild(0).gameObject;
+                    numberText = nowCursor.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+                    beforeCursorNum = nowCursorNum;
+                    nowCursorImage.SetActive(true);
+                }
+
+                //数字プラス
+                if (GameManager.instance.playerInputAction.UI.CursorMoveUp.triggered)
+                {
+                    SoundManager.instance.PlaySE(cursorSE);
+                    passwordNumber[nowCursorNum]++;
+                    if(passwordNumber[nowCursorNum] >= 10) passwordNumber[nowCursorNum] = 0;
+                    numberText.text = passwordNumber[nowCursorNum] + "";
+                    CheckPassword();
+                }
+
+                //数字マイナス
+                if (GameManager.instance.playerInputAction.UI.CursorMoveDown.triggered)
+                {
+                    SoundManager.instance.PlaySE(cursorSE);
+                    passwordNumber[nowCursorNum]--;
+                    if(passwordNumber[nowCursorNum] < 0) passwordNumber[nowCursorNum] = 9;
+                    numberText.text = passwordNumber[nowCursorNum] + "";
+                    CheckPassword();
+                }
+
+                //カール左移動
+                if (GameManager.instance.playerInputAction.UI.CursorMoveLeft.triggered)
+                {
+                    SoundManager.instance.PlaySE(cursorSE);
+                    nowCursorImage.SetActive(false);
+                    nowCursorNum--;
+                    if (nowCursorNum < 0) nowCursorNum = lockedimage.transform.childCount - 1;
+                }
+
+                //カーソル右移動
+                if (GameManager.instance.playerInputAction.UI.CursorMoveRight.triggered)
+                {
+                    SoundManager.instance.PlaySE(cursorSE);
+                    nowCursorImage.SetActive(false);
+                    nowCursorNum++;
+                    if (nowCursorNum >= lockedimage.transform.childCount) nowCursorNum = 0;
+                }
+            }
+        }
         
+    }
+
+    private void CheckPassword()
+    {
+        int count = 0;
+        for(int i  = 0;i < lockedimage.transform.childCount;i++)
+        {
+            if(passwordNumber[i] == answerNumber[i]) count++;
+            else break;
+        }
+        if(count == lockedimage.transform.childCount)
+        {
+            SoundManager.instance.PlaySE(openSE);
+            Debug.Log("当たり");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        inPlayer = true;
+        operation.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        inPlayer = false;
+        operation.SetActive(false);
     }
 }
