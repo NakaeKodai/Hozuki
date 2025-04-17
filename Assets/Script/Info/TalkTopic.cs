613e21db9c1a8d356d43af37761663b122e108a4
NakaeKodai <0uz047526ky3k4d@au.com>
2025-04-18 01:06:11 +0900

    タイムライン追加(一時保存)

--- Assets/Script/Info/TalkTopic.cs
+++ Assets/Script/Info/TalkTopic.cs
@@ -6,9 +6,6 @@ using TMPro;
 
 public class TalkTopic : MonoBehaviour
 {
-    [Header("調べて話すことができるオブジェクトにアタッチしていたらTrue")]
-    public bool isTalkObject = false;
-
     [Header("会話内容　※一行で収まるように※")]
     [SerializeField]
     public List<Topic> topicList = new List<Topic>(); //stringのListのList
@@ -17,65 +14,5 @@ public class TalkTopic : MonoBehaviour
     public class Topic
     {
         public List<string> topic = new List<string>(); //stringのList
-    }
-
-    private bool canTalk;
-
-    [SerializeField] private TalkManager talkManager;
-    [SerializeField] private GameObject operation;
-    [SerializeField] private TextMeshProUGUI operationText;
-
-    void Update()
-    {
-        if(isTalkObject)
-        {
-            if(canTalk && !GameManager.instance.isOtherMenu)
-            {
-                //操作説明
-                if(GameManager.controllerType == GameManager.ControllerType.Unknown)
-                {
-                    operationText.text = "Space : 調べる";
-                }
-                else if(GameManager.controllerType == GameManager.ControllerType.PlayStation)
-                {
-                    operationText.text = "● : 調べる";
-                }
-                else if(GameManager.controllerType == GameManager.ControllerType.Nintendo)
-                {
-                    operationText.text = "A : 調べる";
-                }
-                else if(GameManager.controllerType == GameManager.ControllerType.Xbox)
-                {
-                    operationText.text = "B : 調べる";
-                }
-
-                if(!operation.activeSelf) operation.SetActive(true);
-
-
-                if(GameManager.instance.playerInputAction.Player.ActionANDDecision.triggered)
-                {
-                    talkManager.Talk(topicList[0].topic, true);
-                }
-            }
-        }
-    }
-
-
-    private void OnTriggerEnter2D(Collider2D player)
-    {
-        if (player.gameObject.CompareTag("Player"))
-        {
-            canTalk = true;
-            operation.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D player)
-    {
-        if (player.gameObject.CompareTag("Player"))
-        {
-            canTalk = false;
-            operation.SetActive(false);
-        }
     }
 }
