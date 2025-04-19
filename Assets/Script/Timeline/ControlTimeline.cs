d80bd1a779271c87b48c388d0efb66e39206d4f2
NakaeKodai <0uz047526ky3k4d@au.com>
2025-04-19 19:33:39 +0900

    知力　タイムラインアニメーション追加
    あと少しで知力が終わると思う

--- Assets/Script/Timeline/ControlTimeline.cs
+++ Assets/Script/Timeline/ControlTimeline.cs
@@ -8,18 +8,33 @@ public class ControlTimeline : MonoBehaviour
     public PlayableDirector director;
     private bool waitingForInput = false;
 
+    public Animator targetAnimator;
+    public RuntimeAnimatorController defaultController;
+
     void Update()
     {
         if(GameManager.instance.playerInputAction.UI.DecisionMenu.triggered && waitingForInput)
         {
             director.Resume();
+
+            if(targetAnimator != null && defaultController != null)
+            {
+                targetAnimator.runtimeAnimatorController = defaultController;
+            }
             waitingForInput = false;
         }
     }
 
     public void puase()
     {
+        Debug.Log("ポーズ");
+
+        if(targetAnimator != null)
+        {
+            targetAnimator.runtimeAnimatorController = null;
+        }
         director.Pause();
+        director.Evaluate(); 
         waitingForInput = true;
     }
 }
