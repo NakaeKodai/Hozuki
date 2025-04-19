b867246a855bc2744110f4cedc5fedcc75ec52e1
NakaeKodai <0uz047526ky3k4d@au.com>
2025-04-19 23:23:59 +0900

    競合解消＆タイトルシーン追加

---
+++ Assets/Script/Title/TitleManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+
+    public string cursorSE = "カーソル移動";
+    public string decisionSE = "決定";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
