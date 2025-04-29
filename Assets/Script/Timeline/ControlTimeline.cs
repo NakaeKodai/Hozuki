e3edfd648d863ccc2c11c067a083ee0afa740f3d
NakaeKodai <0uz047526ky3k4d@au.com>
2025-04-29 16:49:09 +0900

    FirstEncount　編集

--- Assets/Script/Timeline/ControlTimeline.cs
+++ Assets/Script/Timeline/ControlTimeline.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
+[System.Serializable]
+public class TimelineControlAnimation
+{
+    public Animator targetAnimator;
+    public RuntimeAnimatorController defaultController;
+}
+
 public class ControlTimeline : MonoBehaviour
 {
     public PlayableDirector director;
     private bool waitingForInput = false;
+
+    public List<TimelineControlAnimation> timelineControlAnimation = new List<TimelineControlAnimation>();
 
     public Animator targetAnimator;
     public RuntimeAnimatorController defaultController;
@@ -17,9 +26,12 @@ public class ControlTimeline : MonoBehaviour
         {
             director.Resume();
 
-            if(targetAnimator != null && defaultController != null)
+            for(int i = 0;i < timelineControlAnimation.Count;i++)
             {
-                targetAnimator.runtimeAnimatorController = defaultController;
+                if(timelineControlAnimation[i].targetAnimator != null && timelineControlAnimation[i].defaultController != null)
+                {
+                    timelineControlAnimation[i].targetAnimator.runtimeAnimatorController = timelineControlAnimation[i].defaultController;
+                }
             }
             waitingForInput = false;
         }
@@ -29,9 +41,12 @@ public class ControlTimeline : MonoBehaviour
     {
         Debug.Log("ポーズ");
 
-        if(targetAnimator != null)
+        for(int i = 0;i < timelineControlAnimation.Count;i++)
         {
-            targetAnimator.runtimeAnimatorController = null;
+            if(targetAnimator != null)
+            {
+                timelineControlAnimation[i].targetAnimator.runtimeAnimatorController = null;
+            }
         }
         director.Pause();
         director.Evaluate(); 
