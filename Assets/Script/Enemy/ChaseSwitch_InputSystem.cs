d50a670ffc6a315aac7a1e031a9fa6a5f340039e
NakaeKodai <0uz047526ky3k4d@au.com>
2025-05-02 20:29:56 +0900

    シン・ファーストエンカウント(一定時間後に追いかけられるようにした)

--- Assets/Script/Enemy/ChaseSwitch_InputSystem.cs
+++ Assets/Script/Enemy/ChaseSwitch_InputSystem.cs
@@ -8,6 +8,12 @@ public class ChaseSwitch_InputSystem : MonoBehaviour
 
     private bool inPlayer;
 
+    [Header("追う時標的をリセットするならTrue")]
+    public bool reset;
+
+    [Header("追うまでの猶予時間(秒)　無いなら0でOK")]
+    public int waitTime;
+
     [Header("追われるようにするならTrue、追われるのをやめるならFalse")]
     public bool cahseSwitch;
     public GameObject ghost;
@@ -30,16 +36,24 @@ public class ChaseSwitch_InputSystem : MonoBehaviour
     void Update()
     {
         
-        if(GameManager.instance.playerInputAction.Player.ActionANDDecision.triggered)
+        if(inPlayer && GameManager.instance.playerInputAction.Player.ActionANDDecision.triggered)
         {
             if(cahseSwitch)
             {
-                Debug.Log("チェイス開始");
-                GameManager.instance.isChaseTime = true;
-                chaseManager.ResetTarget();
-                SoundManager.instance.PlayChaseBGM();
-                ghostNav.speed = speed;
-                ghost.SetActive(true);
+                if(waitTime <= 0)
+                {
+                    GameManager.instance.isChaseTime = true;
+                    Debug.Log("チェイス開始");
+                    if(reset) chaseManager.ResetTarget();
+                    SoundManager.instance.PlayChaseBGM();
+                    ghostNav.speed = speed;
+                    ghost.SetActive(true);
+                    if(onlyOnce) Destroy(gameObject);
+                }
+                else
+                {
+                    StartCoroutine(WaitChase(waitTime));
+                }
             }
             else
             {
@@ -48,10 +62,21 @@ public class ChaseSwitch_InputSystem : MonoBehaviour
                 chaseManager.MoveLocation(ghostX,ghostY);
                 SoundManager.instance.StopBGM();
                 ghost.SetActive(false);
+                if(onlyOnce) Destroy(gameObject);
             }
-
-            if(onlyOnce) Destroy(gameObject);
         }
+    }
+
+    private IEnumerator WaitChase(int seconds)
+    {
+        if(reset) chaseManager.ResetTarget();
+        SoundManager.instance.PlayChaseBGM();
+        ghostNav.speed = speed;
+        ghost.SetActive(true);
+        yield return new WaitForSeconds(seconds);
+
+        GameManager.instance.isChaseTime = true;
+        if(onlyOnce) Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
