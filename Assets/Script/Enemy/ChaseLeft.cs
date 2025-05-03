403fa88267e3444f35fbe972b6f8dd1ab537811d
FUNA1116 <153796025+FUNA1116@users.noreply.github.com>
2025-05-04 00:39:28 +0900

    筋力タイムライン系完了

--- Assets/Script/Enemy/ChaseLeft.cs
+++ Assets/Script/Enemy/ChaseLeft.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class ChaseLeft : MonoBehaviour
 {
     public float speed = 2.5f;
+    public bool canMove = false;
+    Animator animator; //アニメーション変数
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if(canMove)
+        {
+            MoveAnimator(1,0);
+            transform.position -= Vector3.right*speed*Time.deltaTime;
+        }
         
+    }
+
+    public void MoveAnimator(float x, float y)
+    {
+        if (x != 0 || y != 0)
+        {
+            animator.SetFloat("InputX", x);
+            animator.SetFloat("InputY", y);
+        }
     }
 }
